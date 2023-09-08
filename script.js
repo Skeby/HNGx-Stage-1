68816d84099fc8ff1c398b417e7e1f51d5abc487
Adeyinka <akinsanyaadeyinka4166@gmail.com>
2023-09-08 11:41:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var elementCurrentDay = document.querySelector("#currentDayOfTheWeek");
+var elementCurrentUTCTime = document.getElementById("currentUTCTime");
+const currentDate = new Date();
+
+elementCurrentDay.textContent = getCurrentDayOfTheWeek();
+elementCurrentUTCTime.textContent = getCurrentUTCTime();
+
+function getCurrentDayOfTheWeek() {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDayOfTheWeek = daysOfWeek[currentDate.getUTCDay()];
+    return currentDayOfTheWeek;
+}
+
+function getCurrentUTCTime() {
+    const currentUTCTime = currentDate.getUTCMilliseconds();
+    return currentUTCTime;
+}
